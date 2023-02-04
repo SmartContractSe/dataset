569c3f7d5219221e3ff4266f17015e3e0ac4a0a0
SmartContractSe <smartcontractsec@outlook.com>
2023-02-04 12:05:17 +0800

    udpate

--- dataset/non/655C072286A8ab7B33B16Aa6908CFECDe39E57a1.sol
+++
@@ -1,9 +0,0 @@
-/**
- *Submitted for verification at Etherscan.io on 2017-10-25
-*/
-
-pragma solidity ^0.4.18;
-
-contract DPPBurn {
-    
-}
--- dataset/non/7b9e81ec3591b570aa2c9fbec77f7dda76ac4f6d.sol
+++
@@ -1,9 +0,0 @@
-/**
- *Submitted for verification at Etherscan.io on 2016-08-15
-*/
-
-contract Eater{
-	function(){
-		
-	}
-}
