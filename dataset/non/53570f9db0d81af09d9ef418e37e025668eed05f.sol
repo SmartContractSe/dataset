b30059e9d81372abc61765ae2f35662fbd3d9951
SmartContractSe <smartcontractsec@outlook.com>
2023-02-04 12:06:19 +0800

    udpate

---
+++ dataset/non/53570f9db0d81af09d9ef418e37e025668eed05f.sol
@@ -0,0 +1,19 @@
+/**
+ *Submitted for verification at Etherscan.io on 2017-10-19
+*/
+
+pragma solidity ^0.4.0;
+contract whoSays {
+
+    string public name = "whoSays";
+
+    mapping(address => bytes) public data;
+
+    event Said(address indexed person, bytes message);
+
+    function saySomething(bytes _data) public {
+        data[msg.sender] = _data;
+        Said(msg.sender, _data);
+    }
+
+}
---
+++ dataset/non/895d55ce51760ca614b5b446a504c4a49e2d26e8.sol
@@ -0,0 +1,31 @@
+/**
+ *Submitted for verification at Etherscan.io on 2018-01-25
+*/
+
+pragma solidity ^0.4.19;
+
+contract mortal {
+    /* Define variable owner of the type address */
+    address owner;
+
+    /* This function is executed at initialization and sets the owner of the contract */
+    function mortal() { owner = msg.sender; }
+
+    /* Function to recover the funds on the contract */
+    function kill() { if (msg.sender == owner) selfdestruct(owner); }
+}
+
+contract ObjectiveStorage is mortal {
+    address creator;
+    string objective;
+    
+    function ObjectiveStorage(string _objective) public
+    {
+        creator = msg.sender;
+        objective = _objective;
+    }
+    /* Main function */
+    function getObjective() public constant returns (string) {
+        return objective;
+    }
+}
